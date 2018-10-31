b33febe52aaef54b6a336a7b79596cbbb1db0133
Jerome Simeon <jeromesimeon@me.com>
2018-10-31 13:02:34 -0400

    (fix) Update About page
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -995,20 +995,21 @@ class FormContainer extends Component {
         );
         const ModalAbout = () => (
             <Modal trigger={<Menu.Item><Icon name='question'/> About</Menu.Item>}>
-              <Modal.Header>Accord Project &middot; Template Studio (Experimental)</Modal.Header>
+              <Modal.Header>Accord Project &middot; Template Studio</Modal.Header>
               <Modal.Content>
                 <Modal.Description>
                   <Header>Welcome!</Header>
-                  <p>This template studio lets you edit and execute legal contract templates built with the <a href='https://accordproject.org/' target='_blank'>Accord Project</a>.</p>
+                  <p>This template studio lets you load, edit and test legal clause or contract templates built with the <a href='https://accordproject.org/' target='_blank'>Accord Project</a> technology.</p>
                   <p>It is open-source and under active development. Contributions and bug reports are welcome on <a href='https://github.com/accordproject/template-studio' target='_blank'>GitHub</a>.</p>
                 </Modal.Description>
                 <Divider/>
                 <Modal.Description>
                   <Header>Getting started</Header>
                   <p>Search a template from the <a href='https://templates.accordproject.org' target='_blank'>Accord Project template library</a> (Search box at the top)</p>
-                  <p>Chose whether to edit the Contract Text or the Logic (Tab on the left)</p>
-                  <p>Edit the contract template and try it on a sample contract</p>
-                  <p>Edit the Ergo contract logic and try it by executing a simple request</p>
+                  <p>Chose whether to edit the <b>Contract Text</b>, <b>the Model</b>, or the <b>Logic</b> (Tab on the upper left)</p>
+                  <p>Edit the <b>Test Contract</b> and inspect the corresponding <b>Contract data</b></p>
+                  <p>Edit the <b>Template</b> and see the corresponding <b>Test Contract</b></p>
+                  <p>Edit the <b>Ergo</b> contract logic and test it by executing requests</p>
                 </Modal.Description>
                 <Divider/>
                 <Modal.Description>
