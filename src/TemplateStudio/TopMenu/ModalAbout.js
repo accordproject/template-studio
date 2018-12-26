66f6eaecf166f61e80692c0367d489cda50a7633
Diana Lease <dianarlease@gmail.com>
2018-12-26 10:19:46 -0500

    starts updating files according to linting rules
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/TemplateStudio/TopMenu/ModalAbout.js
+++ src/TemplateStudio/TopMenu/ModalAbout.js
@@ -12,10 +12,7 @@
  * limitations under the License.
  */
 
-'use strict';
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Divider, Header, Icon, Menu, Modal } from 'semantic-ui-react';
 import * as ciceroPackageJson from '@accordproject/cicero-core/package.json';
 import * as ergoPackageJson from '@accordproject/ergo-compiler/package.json';
@@ -23,19 +20,19 @@ import * as ergoPackageJson from '@accordproject/ergo-compiler/package.json';
 const ciceroVersion = ciceroPackageJson.version;
 const ergoVersion = ergoPackageJson.version;
 
-const ModalAbout = (props) => (
-  <Modal trigger={<Menu.Item><Icon name='question'/> About</Menu.Item>}>
+const ModalAbout = () => (
+  <Modal trigger={<Menu.Item><Icon name="question" /> About</Menu.Item>}>
     <Modal.Header>Accord Project &middot; Template Studio</Modal.Header>
     <Modal.Content>
       <Modal.Description>
         <Header>Welcome!</Header>
-        <p>This template studio lets you load, edit and test legal clause or contract templates built with the <a href='https://accordproject.org/' target='_blank'>Accord Project</a> technology.</p>
-        <p>It is open-source and under active development. Contributions and bug reports are welcome on <a href='https://github.com/accordproject/template-studio' target='_blank'>GitHub</a>.</p>
+        <p>This template studio lets you load, edit and test legal clause or contract templates built with the <a href="https://accordproject.org/" target="_blank" rel="noopener noreferrer">Accord Project</a> technology.</p>
+        <p>It is open-source and under active development. Contributions and bug reports are welcome on <a href="https://github.com/accordproject/template-studio" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
       </Modal.Description>
-      <Divider/>
+      <Divider />
       <Modal.Description>
         <Header>Getting started</Header>
-        <p>Search a template from the <a href='https://templates.accordproject.org' target='_blank'>Accord Project template library</a> (Search box at the top)</p>
+        <p>Search a template from the <a href="https://templates.accordproject.org" target="_blank" rel="noopener noreferrer">Accord Project template library</a> (Search box at the top)</p>
         <p>Chose whether to edit the <b>Contract Text</b>, <b>the Model</b>, or the <b>Logic</b> (Tab on the upper left)</p>
         <p>Edit the <b>Test Contract</b> and inspect the corresponding <b>Contract data</b></p>
         <p>Edit the <b>Template</b> and see the corresponding <b>Test Contract</b></p>
--- src/TemplateStudio/TopMenu/ModalUpload.js
+++ src/TemplateStudio/TopMenu/ModalUpload.js
@@ -12,21 +12,22 @@
  * limitations under the License.
  */
 
-'use strict';
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Header, Icon, Menu, Modal } from 'semantic-ui-react';
 
 import FileUpload from '../FileUpload';
 
-const ModalUpload = (props) => (
-  <Modal size='small' open={props.modalUploadOpen}
-         onClose={props.handleUploadClose}
-         trigger={<Menu.Item onClick={props.handleUploadOpen}><Icon name='upload'/>...from disk</Menu.Item>}>
+const ModalUpload = props => (
+  <Modal
+    size="small"
+    open={props.modalUploadOpen}
+    onClose={props.handleUploadClose}
+    trigger={<Menu.Item onClick={props.handleUploadOpen}><Icon name="upload" />...from disk</Menu.Item>}
+  >
     <Header>Upload a template archive (.cta file) from your machine:</Header>
     <Modal.Content>
-      <FileUpload handleUploadConfirm={props.handleUploadConfirm}/>
+      <FileUpload handleUploadConfirm={props.handleUploadConfirm} />
     </Modal.Content>
   </Modal>
 );
@@ -36,6 +37,6 @@ ModalUpload.propTypes = {
   handleUploadConfirm: PropTypes.func.isRequired,
   handleUploadOpen: PropTypes.func.isRequired,
   modalUploadOpen: PropTypes.bool.isRequired,
-}
+};
 
 export default ModalUpload;
